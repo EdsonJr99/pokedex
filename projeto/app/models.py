930c8bef6d383d2bca6c2386c6727bd0933e14e9
IzaiasZIPL <Izaiasjrterto@gmail.com>
2025-09-18 16:54:50 -0300

    Update models.py

--- projeto/app/models.py
+++ projeto/app/models.py
@@ -1,3 +1,9 @@
 from django.db import models
 
-# Create your models here.
+class Pokemon(models.Model):
+    nome = models.CharField(max_length=30)
+    height = models.IntegerField()
+    weight = models.IntegerField()
+
+    def __str__(self):
+        return self.nome
