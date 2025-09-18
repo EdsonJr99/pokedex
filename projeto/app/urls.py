ebee0cf1242dd78f6f3d5e13768d890485e7e714
IzaiasZIPL <Izaiasjrterto@gmail.com>
2025-09-18 16:53:18 -0300

    Update urls.py

--- projeto/app/urls.py
+++ projeto/app/urls.py
@@ -1,8 +1,11 @@
-from django.urls import path, include
-from .views import 
-
+from django.urls import path
+from . import views
+from .views import listar_pokemons, criar_pokemon, deletar_pokemon, atualizar_pokemon
 
 urlpatterns = [
-    path('', home, name = 'home'),
-
+    path('', views.home, name='home'),
+    path('listar_pokemons', listar_pokemons, name='listar_pokemons'),
+    path('criar_pokemon', criar_pokemon, name='criar_pokemon'),
+    path('delete/<int:pk>', deletar_pokemon, name='deletar_pokemon'),
+    path('atualizar/<int:pk>', atualizar_pokemon, name='atualizar_pokemon' ),
 ]
