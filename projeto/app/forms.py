388034e630f53015c991da70792cf7b91c4d7196
IzaiasZIPL <Izaiasjrterto@gmail.com>
2025-09-18 17:46:44 -0300

    feat(views): adiciona views e forms

--- projeto/app/forms.py
+++ projeto/app/forms.py
@@ -5,3 +5,8 @@ class PokemonForm(forms.ModelForm):
     class Meta:
         model = Pokemon
         fields = ['nome','height','weight']
+
+class TreinadorForm(forms.ModelForm):
+    class Meta:
+        model = Pokemon
+        fields = ['nome','idade','genero', 'main_pokemon']
--- projeto/app/views.py
+++ projeto/app/views.py
@@ -1,6 +1,6 @@
 from django.shortcuts import render, redirect
 from .models import Pokemon
-from .forms import PokemonForm
+from .forms import TreinadorForm
 
 def home(request):
     return render(request, 'home.html')
@@ -29,10 +29,42 @@ def deletar_pokemon(request, pk):
 def atualizar_pokemon(request, pk):
     pokemon = Pokemon.objects.get(pk=pk)
     if request.method == 'POST':
-        form = PokemonForm(request.POST, instance=pokemon)
+        form = TreinadorForm(request.POST, instance=pokemon)
         if form.is_valid():
             form.save()
             return redirect('listar_pokemons')
     else:
-        form = PokemonForm(instance=pokemon)
+        form = TreinadorForm(instance=pokemon)
     return render(request, 'criarPok.html', {'pokemon': form})
+
+def listar_treinador(request):
+    treinador = Treinador.objects.all()
+    return render(request, 'listarTreins.html', {'treinador': treinador})
+
+def criar_treinador(request):
+    if request.method == 'POST':
+        form = TreinadorForm(request.POST)
+        if form.is_valid():
+            form.save()
+            return redirect('listar_treinador')
+    else:
+        form = TreinadorForm()
+    return render(request, 'criarTrein.html', {'treinador': form})
+
+def deletar_treinador(request, pk):
+    pokemon = Pokemon.objects.get(pk=pk)
+    if request.method == 'POST':
+        pokemon.delete()
+        return redirect('listar_treinador')
+    return render(request, 'confirmar_deleteTrein.html', {'treinador': treinador})
+
+def atualizar_treinador(request, pk):
+    pokemon = Pokemon.objects.get(pk=pk)
+    if request.method == 'POST':
+        form = PokemonForm(request.POST, instance=pokemon)
+        if form.is_valid():
+            form.save()
+            return redirect('listar_treinador')
+    else:
+        form = PokemonForm(instance=pokemon)
+    return render(request, 'criarTrein.html', {'pokemon': form})
