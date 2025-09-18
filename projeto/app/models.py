7339e4dc429fe5b3adb0b8fe494a2aaed07ff1a5
Edson <jredson525@gmail.com>
2025-09-18 17:13:48 -0300

    Feat: Criando model

--- projeto/app/models.py
+++ projeto/app/models.py
@@ -7,3 +7,15 @@ class Pokemon(models.Model):
 
     def __str__(self):
         return self.nome
+
+class Treinador(models.Model):
+    nome = models.CharField(max_length=35)
+    idade = models.IntegerField()
+    genero = models.CharField(max_length=20)
+    main_pokemon = models.CharField(max_length=25)
+
+    def __str__(self):
+        return self.nome
+
+
+    
